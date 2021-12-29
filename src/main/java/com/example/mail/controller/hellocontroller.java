620d63824d59eb4119b07f061591c03fdf749228
XiaoKe <1261538478@qq.com>
2021-12-29 17:23:03 +0800

    feat

--- src/main/java/com/example/mail/controller/hellocontroller.java
+++ src/main/java/com/example/mail/controller/hellocontroller.java
@@ -17,7 +17,7 @@ public class hellocontroller {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
         System.out.println(username);
-        if(username.equals("abc") && password.equals("111")){
+        if(username.equals("abc") && password.equals("321")){
             return "true";
         }
         return "false";
