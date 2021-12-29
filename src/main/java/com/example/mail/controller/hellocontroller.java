2b5d726d35d11a47354cf310a9833d7ee7fb01a4
jonusis <jonusis@github.com>
2021-12-29 16:55:52 +0800

    feat: 新接口（登陆注册）

---
+++ src/main/java/com/example/mail/controller/hellocontroller.java
@@ -0,0 +1,30 @@
+package com.example.mail.controller;
+import com.sun.net.httpserver.HttpServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping(value = "/v1")
+
+public class hellocontroller {
+
+    @RequestMapping(value = "/login",method=RequestMethod.POST)
+    public String Login(HttpServletRequest req, HttpServletResponse resp){
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        System.out.println(username);
+        if(username.equals("abc") && password.equals("111")){
+            return "true";
+        }
+        return "false";
+    }
+
+    @RequestMapping(value = "/hello")
+    public String hello(){
+        return "hello";
+    }
+}
